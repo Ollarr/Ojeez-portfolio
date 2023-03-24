553854fdc7e9dbd47dc388ed1c128012ec8bbde0
Oluwasegun <oluyedis@gmail.com>
2023-03-24 17:38:53 +0100

    added the experiences section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import About from "./components/About";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
+      <Experience />
     </div>
   );
 }
