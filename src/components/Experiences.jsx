41d4a6d685ccf0428ac773b5b7e70fd5b463a5b7
Oluwasegun <oluyedis@gmail.com>
2023-03-28 17:31:43 +0100

    worked on experience section

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,47 +1,65 @@
 import React from "react";
 
-function Footer() {
-  const footerLinks = [
-    { name: "about", link: "/about" },
-    { name: "projects", link: "/projects" },
-    { name: "contact", link: "/contact" },
+function Experiences() {
+  const workExperiences = [
+    {
+      role: "Backend role",
+      decsription:
+        " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.",
+      time: "May 9th 2018 - August 25th 2018",
+    },
+    {
+      role: "Backend role",
+      decsription:
+        " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.",
+      time: "May 9th 2018 - August 25th 2018",
+    },
+    {
+      role: "Backend role",
+      decsription:
+        " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.",
+      time: "May 9th 2018 - August 25th 2018",
+    },
+    {
+      role: "Backend role",
+      decsription:
+        " Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.",
+      time: "May 9th 2018 - August 25th 2018",
+    },
   ];
   return (
-    <footer class="bg-gray-900 mt-6 ">
-      <div class="w-full container mx-auto p-4 md:px-6 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <a href="/" class="flex items-center mb-4 sm:mb-0">
-            {/* <img src="/" class="h-8 mr-3" alt="footerLogo" /> */}
-            <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-white ">
-              &lt;Ojeez/&gt;
-            </div>
-          </a>
-          <ul class="flex flex-wrap items-center justify-items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            {footerLinks.map((link) => (
-              <li
-                key={link.name}
-                className="md:ml-8 text-md md:my-0 my-7 font-[nunito] capitalize"
-              >
-                <a href={link.link} className="mr-4 md:mr-6">
-                  {link.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023
-        </span>
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          <a href="https://github.com/Ollarr" target="_blank" rel="noreferrer">
-            Made by Dev Ollar™
-          </a>
-          . All Rights Reserved.
-        </span>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          Working Experiences
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
       </div>
-    </footer>
+      <div className="flex flex-col items-center justify-items-center gap-8 ">
+        {workExperiences.map((experience) => {
+          return (
+            <div className="flex items-center">
+              <div className="text-start">
+                <h6 className="mb-2 font-bold text-3xl leading-5">
+                  {experience.role}
+                </h6>
+                <p className="text-sm text-gray-900">
+                  {experience.decsription}
+                </p>
+                <p className="text-sm font-bold text-gray-900">
+                  {experience.time}
+                </p>
+                <hr className="w-full my-6 border-gray-300" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
-export default Footer;
+export default Experiences;
